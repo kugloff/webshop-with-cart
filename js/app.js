5780ebc4ea12b414d9a24dfa6b355475509d2d03
Zsofia Varga <varzso01@gmail.com>
2023-03-29 19:33:00 +0200

    Témazáró - Logó beillesztése + Termékek megjelenítésének esztétikai beállításai

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const navCategories = document.getElementById('categories');
 const categoriesContent = document.getElementById('categoriesContent');
 const categoriesContainer = document.getElementById('categoriesContainer');
 
+const productInfo = document.getElementById('productInfo');
+productsObject = {};
+
 navCategories.addEventListener('click', () => {
     categoriesContent.classList.toggle('active');
 })
@@ -17,13 +20,20 @@ fetch('https://dummyjson.com/products/categories')
             fetch(`https://dummyjson.com/products/category/${category}`)
             .then(res => res.json())
             .then(data => {
+                products = data.products;
+                productsObject = data.products;
                 const categoryTitle = document.getElementById(`${category}`);
                 categoryTitle.addEventListener('click', ()=>{
                     productContainer.innerHTML = "";
-                    products = data.products;
                     data.products.forEach(product => {
                         productContainer.innerHTML +=
-                        `<div>${product.title}</div>`
+                        `<div class="item">
+                            <figure style="height=200px"><img src=${product.images[0]} alt=${product.title}></figure>
+                            <h3>${product.title}</h3>
+                            <p>${product.description}</p>
+                            <div class="price">${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(product.price)}</div>
+                            <button class="addToCartBtn">Add to cart</button>
+                        </div>`
                     })
                 })
             })
@@ -31,6 +41,7 @@ fetch('https://dummyjson.com/products/categories')
     })
     .catch(error => console.log(error));
 
+const addToCartButtons = document.getElementsByClassName('addToCartBtn');
 const miniCartButton = document.getElementById('miniCartButton');
 const totalQty = document.getElementById('totalQty');
 const miniCartContent = document.getElementById('miniCartContent');
