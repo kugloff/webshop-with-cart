f644613e3944dd4b826a0a3248ac9365ada143a5
Zsofia Varga <varzso01@gmail.com>
2023-03-30 12:52:22 +0200

    Témazáró - readme fájl létrehozása + kosárhoz adás működésre bírása

--- js/app.js
+++ js/app.js
@@ -43,86 +43,118 @@ fetch('https://dummyjson.com/products/categories')
                                 <div class="price">${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(product.price)}</div>
                                 <button class="addToCartBtn" data-product-id="${product.id}">Add to cart</button>
                             </div>`
-                        })
+                        })                        
                         let addToCartButtons = document.getElementsByClassName('addToCartBtn')
                         for (const button of addToCartButtons){
                             button.addEventListener('click', ()=>{
-                                if (cart[button.dataset.productId] === undefined) {
-                                    cart[button.dataset.productId] = 1;
+                                const productId = button.dataset.productId;
+                                if (cart[productId] === undefined) {
+                                    cart[productId] = 1;
                                 } else {
-                                    cart[button.dataset.productId]++;
+                                    cart[productId]++;
                                 }
-                                updateCart(cart, data.products);
-                                allQty++;
+
+                                let allQty = 0;
+                                for (const productId in cart) {
+                                allQty += cart[productId];
+                                }
                                 totalQty.innerText = `(${allQty})`;
+
+                                //termék hozzáadása a kosárhoz
+                                if (cart[productId] === undefined) {
+                                    cart[productId] = 1;
+                                }
+                                else {
+                                    cart[productId]++;
+                                }
+                                //termék eltávolítása a kosárból(0)
+                                if (cart[productId] <= 1) {
+                                    delete cart[productId];
+                                }
+                                else {
+                                    cart[productId]--;
+                                }
+
+                                updateCart(cart, data.products);
                             })
                         }
 
-                            function updateCart(cart, categoryProducts) {
-                                miniCartProducts.innerHTML = '';
-                                let total = 0;
-                              
-                                for (const id in cart) {
-                                  let price = categoryProducts.find(product => product.id == id).price;
-                                  let productName = categoryProducts.find(product => product.id == id).title;
-                                  let subTotal = price * cart[id];
-                                  total += subTotal;
-                                  miniCartProducts.innerHTML += `
-                                    <div>
-                                      <h2>${productName}</h2>
-                                      <p>Price: ${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(price)}</p>
-                                      <p>Subtotal: ${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(subTotal)}</p>
-                                      <button class="increase-button" data-product-id="${id}"> + </button> 
-                                      <span>${cart[id]}</span>
-                                      <button class="decrease-button" data-product-id="${id}"> - </button> 
-                                      <hr>
-                                    </div>`
+                        function updateCart(cart, categoryProducts) {
+                            let productsToUpdate = miniCartProducts.querySelectorAll('.item');
+                            let total = 0;
+                          
+                            for (const id in cart) {
+                              let price = 0;
+                              let productName = "";
+                              for (const product of categoryProducts) {
+                                if (product.id == id) {
+                                    price = product.price;
+                                    productName = product.title;
+                                    break;
                                 }
-
-                                miniCartProducts.innerHTML += `<h4> Total: ${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(total)}</h4>`;
-                              
-                                //increaseAmount();
-                                //decreaseAmount();
-                            }               
-                            
-                            const increaseButton = document.getElementsByClassName('increase-button');
-                            const decreaseButton = document.getElementsByClassName('decrease-button');
-
-                            /*function increaseAmount(){
-                                for (let i = 0; i < increaseButton.length; i++) {
-                                    increaseButton[i].addEventListener("click", function (event) {
-                                        if (cart[event.target.dataset.productId] === undefined) {
-                                        cart[event.target.dataset.productId] = 1;
-                                        } else {
-                                        cart[event.target.dataset.productId]++;
-                                        }
-                                        allQty++;
-                                        totalQty.innerText = `(${allQty})`;
-                                        //updateCart(cart, categoryProducts);
-                                    })
+                              }
+                              let subTotal = price * cart[id];
+                              total += subTotal;
+                          
+                              let productToUpdate = Array.from(productsToUpdate).find(p => p.dataset.productId == id);
+                              if (productToUpdate) {
+                                productToUpdate.querySelector('.quantity').textContent = cart[id];
+                              } else {
+                                miniCartProducts.insertAdjacentHTML('beforeend', `
+                                    <div class="item" data-product-id="${id}">
+                                        <h2>${productName}</h2>
+                                        <p>Price: ${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(price)}</p>
+                                        <p>Subtotal: ${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(subTotal)}</p>
+                                        <button class="increase-button" data-product-id="${id}"> + </button> 
+                                        <span class="quantity">${cart[id]}</span>
+                                        <button class="decrease-button" data-product-id="${id}"> - </button> 
+                                        <hr>
+                                    </div>
+                                `);
+                              }
+                            }
+                          
+                            let productsToDelete = miniCartProducts.querySelectorAll('.item');
+                            for (const product of productsToDelete) {
+                                if (!cart[product.dataset.productId]) {
+                                    product.remove();
                                 }
                             }
-
-                            function decreaseAmount(){
-                                for (let i = 0; i < decreaseButton.length; i++) {
-                                    decreaseButton[i].addEventListener("click", function (event) {
-                                        if (cart[event.target.dataset.productId] <= 1) {
-                                        delete cart[event.target.dataset.productId];
-                                        } else {
-                                        cart[event.target.dataset.productId]--;
-                                        }
-                                        allQty--;
-                                        totalQty.innerText = `(${allQty})`;
-                                        //updateCart(cart, categoryProducts);
-                                    })
-                                }
-                            }*/
+                          
+                            miniCartTotal.textContent = `Total: ${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(total)}`;
+                        }
+                          
                     })
                 })
         })                
     })
     .catch(error => console.log(error));
     
+
+const increaseButton = document.getElementsByClassName('increase-button');
+const decreaseButton = document.getElementsByClassName('decrease-button');
+
+/*for (let i=0;i<increaseButton.length;i++) {
+    increaseButton[i].addEventListener("click", () => {
+        const productId = button.dataset.productId;
+
+        cart[productId]++;
+        allQty++;
+        totalQty.innerText = `(${allQty})`;
+        updateCart(cart, data.products);
+    })
+}
+for (let i = 0; i < decreaseButton.length; i++) {
+    decreaseButton[i].addEventListener("click", () => {
+        const productId = button.dataset.productId;
+
+        cart[productId]--;
+        allQty--;
+        totalQty.innerText = `(${allQty})`;
+        updateCart(cart, data.products);
+    })
+}*/
+
 miniCartButton.addEventListener('click', () => {
     miniCartContent.classList.toggle('active');
 })
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Kedves Tanár úr!
+
+Sajnos nem tudtam megoldani, hogy kategória-váltás után is megfelelően működjön a kosárhoz adás.
+
+Ha kategóriát váltunk az oldalon (és már az előző kategóriából raktunk termékeket a kosárba), akkor a termékek **subTotal** értékei és a **total** érték "lenullázódik".
+
+**DE,**
+
+az elkövetkezendő napokban még gondolkozni fogok és megpróbálok megoldást találni a problémára.
+
+Ez a témazáró nagyon szuper volt ahhoz, hogy az eddigi összes tudásunkat a javascript-ről felelevenítsük.
+
